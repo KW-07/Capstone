3a07b50cbfe529471d762b3f4160fce57247b22e
OwenEden <parksh1323@gmail.com>
2024-12-31 20:46:42 +0900

    Save
    설명을 만드는 것은 너무 어려워

--- Assets/Capstone/Scripts/Player/PlayerAttack.cs
+++ Assets/Capstone/Scripts/Player/PlayerAttack.cs
@@ -8,6 +8,9 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private GameObject normalProjectile;
     [SerializeField] private Transform shootPoint;
 
+    public Transform pos;
+    public Vector2 boxSize;
+    public int damage;
 
     private void Start()
     {
@@ -19,5 +22,27 @@ public class PlayerAttack : MonoBehaviour
             {
                 Instantiate(normalProjectile, shootPoint.position, shootPoint.rotation);
             }
+    }
+
+    public void OnMeleeAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+            foreach (Collider2D collider in collider2Ds)
+            {
+                if (collider.tag == "Enemy")
+                {
+
+                }
+            }    
+            Debug.Log("attack");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(pos.position, boxSize);
     }
 }
