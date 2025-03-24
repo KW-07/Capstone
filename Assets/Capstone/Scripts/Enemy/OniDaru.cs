f3e4f340519bcb0504653b285f4f3ce542b61cc2
OwenEden <parksh1323@gmail.com>
2025-03-24 23:22:22 +0900

    Re: add monstor

--- Assets/Capstone/Scripts/Enemy/OniDaru.cs
+++ Assets/Capstone/Scripts/Enemy/OniDaru.cs
@@ -1,106 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
-public class OniDaru : MonoBehaviour
+public class OniDaru : LivingEntity
 {
     public Transform playerTransform;
+
+    [Header("Range")]
     public float attackRange = 1.5f;
     public float detectionRange = 5.0f; // 플레이어를 감지하는 거리
 
+    [Header("Itemdrop")]
     public bool ItemDrop;
-    public float maxHealth = 100f;
-    private float currentHealth;
 
     public float moveSpeed = 2.0f;
 
-    public Vector2 attack1BoxSize;
+    [Header("Attack")]
+    public Transform attackPoint;
+    public Vector2 attackBoxSize;
     public float attackDelay = 1.0f;  // 공격의 선딜? 아래의 쿨다운과는 다른것
     public float attackCooldown = 2.0f;
     private float nextAttackTime = 0f;
 
-    public int nextMoveTime = 3;
+    public int nextThinkTime = 3;
     private int nextMove;
 
+    private Animator animator;
     private Rigidbody2D rb;
-
-    private bool isDead = false;
-
     private BTSelector root;
 
     private void Start()
     {
-        currentHealth = maxHealth;
         rb = GetComponent<Rigidbody2D>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        Invoke("Think", nextMoveTime);
+        Invoke("Think", nextThinkTime);
 
         root = new BTSelector();
 
         BTSequence attackSequence = new BTSequence();
+        attackSequence.AddChild(new BTCondition(IsPlayerInRange));
+        attackSequence.AddChild(new BTCondition(CanAttack));
+        attackSequence.AddChild(new BTAction(Attack));
+
         BTSequence chaseSequence = new BTSequence();
+        chaseSequence.AddChild(new BTCondition(IsPlayerDetected));
+        chaseSequence.AddChild(new BTAction(Chase));
+
         BTSequence patrolSequence = new BTSequence();
+        patrolSequence.AddChild(new BTAction(Patrol));
+
         BTSequence deathSequence = new BTSequence();
-
-        BTAction attack = new BTAction(Attack);
-        BTAction chaseAction = new BTAction(Chase);
-        BTAction patrolAction = new BTAction(Patrol);
-        BTAction dieAction = new BTAction(Die);
-
-        BTCondition playerInRange = new BTCondition(IsPlayerInRange);
-        BTCondition playerDetected = new BTCondition(IsPlayerDetected);
-        BTCondition isdeadCondition = new BTCondition(() => isDead);
-        BTCondition canAttack = new BTCondition(CanAttack);;
-
-        attackSequence.AddChild(playerInRange);
-        attackSequence.AddChild(canAttack);
-        attackSequence.AddChild(attack);
-
-        chaseSequence.AddChild(playerDetected);
-        chaseSequence.AddChild(chaseAction);
-
-        patrolSequence.AddChild(patrolAction);
-
-        deathSequence.AddChild(isdeadCondition);
-        deathSequence.AddChild(dieAction);
+        deathSequence.AddChild(new BTCondition(() => dead));
+        deathSequence.AddChild(new BTAction(Die));
 
         root.AddChild(deathSequence);
         root.AddChild(attackSequence);
         root.AddChild(chaseSequence);
         root.AddChild(patrolSequence);
-
-        root.Evaluate();
     }
 
     private void Update()
     {
         root.Evaluate();
     }
-
-    private bool IsPlayerInRange()
+    private void FixedUpdate()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
-        return distanceToPlayer <= attackRange;
+        Vector2 frontVec = new Vector2(rb.position.x + nextMove * 0.5f, rb.position.y);
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Ground"));
+        if (rayHit.collider == null)
+        {
+            nextMove *= -1;
+            CancelInvoke();
+            Invoke("Think", nextThinkTime);
+        }
     }
 
-    private bool IsPlayerDetected()
-    {
-        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
-        return distanceToPlayer <= detectionRange;
-    }
-
-    private bool CanAttack()
-    {
-        return Time.time >= nextAttackTime;
-    }
-    private void SetNextAttackTime()
-    {
-        nextAttackTime = Time.time + attackCooldown;
-    }
+    private bool IsPlayerInRange() => Vector3.Distance(transform.position, playerTransform.position) <= attackRange;
+    private bool IsPlayerDetected() => Vector3.Distance(transform.position, playerTransform.position) <= detectionRange;
+    private bool CanAttack() => Time.time >= nextAttackTime;
+    private void SetNextAttackTime() => nextAttackTime = Time.time + attackCooldown;
 
     #region attack
     private BTNodeState Attack()
     {
+        LookAtPlayer();
         Debug.Log("Preparing Attack...");
         StartCoroutine(DelayedAttack());
         SetNextAttackTime();
@@ -112,23 +96,18 @@ public class OniDaru : MonoBehaviour
 
         // 실제 공격 실행 (전방 공격)
         Debug.Log("Performing Attack after delay!");
-
-        // 여기에 전방 공격 로직 추가 (예: 히트박스 활성화, 애니메이션 트리거)
         PerformForwardAttack();
     }
     private void PerformForwardAttack()
     {
         // 전방 공격 로직 (예: 히트박스 검사)
-        Vector2 attackPosition = transform.position + transform.right * 1.5f; // 캐릭터 앞쪽
-
-        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPosition, attack1BoxSize, 0);
+        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPoint.position, attackBoxSize, 0);
         foreach (var enemy in hitEnemies)
         {
             if (enemy.CompareTag("Player"))
             {
                 Debug.Log("Hit Player!");
-                //enemy.GetComponent<Player>().TakeDamage(10);
-                // 여기에 플레이어에게 피해를 주는 코드 추가
+                enemy.GetComponent<LivingEntity>().OnDamage(10);
             }
         }
     }
@@ -141,6 +120,7 @@ public class OniDaru : MonoBehaviour
             //Debug.Log("Player is in attack range, stopping chase.");
             return BTNodeState.Failure;
         }
+        LookAtPlayer();
         transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
         return BTNodeState.Running;
     }
@@ -156,22 +136,42 @@ public class OniDaru : MonoBehaviour
     private void Think()
     {
         nextMove = Random.Range(-1, 2);
-
-        Invoke("Think", nextMoveTime);
+        transform.localScale = new Vector3(nextMove == -1 ? -1 : 1, 1, 1);
+        Invoke("Think", nextThinkTime);
     }
     private BTNodeState Die()
     {
+        OnDie();
         return BTNodeState.Success;
     }
-    public void TakeDamage(float damage)
+    public override void OnDamage(float damage)
     {
-        currentHealth -= damage;
-        Debug.Log("Monster took damage! Current Health: " + currentHealth);
-
-        if (currentHealth <= 0)
+        //Debug.Log("Monster took damage! Current Health: " + currentHealth);
+        base.OnDamage(damage);
+    }
+    public override void OnDie()
+    {
+        base.OnDie();
+        rb.velocity = Vector2.zero;  // 움직임 정지
+        GetComponent<Collider2D>().enabled = false;  // 충돌 제거
+        Destroy(this.gameObject);
+    }
+    private void LookAtPlayer()
+    {
+        if (playerTransform == null) return;
+        transform.localScale = new Vector3(playerTransform.position.x < transform.position.x ? -1 : 1, 1, 1);
+    }
+    private void OnDrawGizmos()
+    {
+        if (attackPoint != null)
         {
-            isDead = true;
-            Debug.Log("Monster has died!");
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(attackPoint.position, attackBoxSize);
         }
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
     }
 }
