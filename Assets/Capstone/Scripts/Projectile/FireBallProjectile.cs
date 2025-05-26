c20a1d57d7293771d9985a362c3905cd12c41385
OwenEden <parksh1323@gmail.com>
2025-05-27 03:33:35 +0900

    Add Anim
    BT 트리에 약간의 문제가 있다...
    고민해봐야할듯

--- Assets/Capstone/Scripts/Projectile/FireBallProjectile.cs
+++ Assets/Capstone/Scripts/Projectile/FireBallProjectile.cs
@@ -5,29 +5,25 @@ using UnityEngine;
 public class FireBallProjectile : MonoBehaviour
 {
     public float damage = 10f;
-    public float lifetime = 5f;
 
-    private void Start()
+    private Animator animator;
+    private void Awake()
     {
-        Destroy(gameObject, lifetime); // 일정 시간 후 자동 파괴 (예: 화면 밖으로 날아간 경우)
+        animator = GetComponent<Animator>();
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            // 플레이어에게 데미지를 줌
             LivingEntity player = collision.GetComponent<LivingEntity>();
             if (player != null)
             {
                 player.OnDamage(damage);
             }
-
-            Destroy(gameObject); // 충돌 후 파괴
+            Destroy(gameObject);
         }
         else if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            // 땅에 닿으면 파괴 (원하면)
             Destroy(gameObject);
         }
     }
